abd42230e4364ad61273c7b97f10d585b95d4fcb
Tumi Snær Gíslason <tumsgis@gmail.com>
2015-11-08 21:14:53 +0000

    smá lagfæring

--- NonWordErrors.R
+++ NonWordErrors.R
@@ -1,15 +1,22 @@
+############################################################################
+# data is the csv file to find non word errors in
+#
+# Authors: Kjartan Marteinsson, Snorri Ágúst Snorrason, Tumi Snær Gíslason.
+############################################################################
 findNonWordErrors <- function(data, csv=FALSE) {
   
   # Read in the csv file if we haven't done that already
   if (csv) {
     data <- read.csv(data, encoding='UTF-8')
     data <- subset(data, grepl('^[[:alpha:]]+$', data$Word))
-    # data <- lapply(data, tolower)
+    data$Word <- as.character(data$Word)
+    data$CorrectWord <- as.character(data$CorrectWord)
+    data$Word <- lapply(data$Word, tolower)
   }
   
   # Add the correction of the word to the CorrectWord column
   lengthData <- length(data$Word)
-  for (i in 1:100) {
+  for (i in 1:20) {
     tmpWord <- data$Word[[i]]
     
     if (!(tmpWord %in% dictionary)) {
@@ -22,6 +29,7 @@ findNonWordErrors <- function(data, csv=FALSE) {
       minEditDistWords <- subset(editDistDf, EditDist == minEditDist, select=c('Word')) 
       
       # Pick the word having the most prob of showing up, if we have more than one word.
+      # Otherwise we just use the word itself.
       if (length(minEditDistWords) > 1) {
         maxFreq <- 0
         maxFreqWord <- ''
@@ -34,10 +42,10 @@ findNonWordErrors <- function(data, csv=FALSE) {
         }
         tmpWord <- maxFreqWord
       }
-      else {
-        tmpWord <- minEditDistWords
-      }
+      else
+        tmpWord <- as.character(minEditDistWords$Word)
     }
+    
     # Update the CorrectWord column in the data frame
     data[i, 4] <- tmpWord
   }
