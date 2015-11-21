c3988cf67063c61dc74d14a7e955efd8597fc82a
KjartanMa <kjartanm2@gmail.com>
2015-11-21 19:29:55 +0000

    Nota bara lemmur

--- CreateRealDictionary.R
+++ CreateRealDictionary.R
@@ -16,42 +16,45 @@ if(exists('dictionaryTag')){
   rm(dictionaryTag)
 }
 
-if(exists('dictionaryWord')){
-  rm(dictionaryWord)
+if(exists('dictionaryLemma')){
+  rm(dictionaryLemma)
 }
 
+if(exists('dictionaryLink')){
+  rm(dictionaryLink)
+}
 # Read all the data files and create a dictionary out of the words
 # contained in them.
-for (name in files) {
-  new <- read.csv(paste0('althingi_tagged/', name), encoding = 'UTF-8', colClasses = c('character', 'character', 'character'))
+for (name in files[1:15]) {
+  print(name)
+  new <- read.csv(paste0('althingi_tagged/', name), encoding = 'UTF-8', stringsAsFactors = FALSE)
   # Remove all non letter words.
   new <- subset(new, grepl('(^[[:alpha:]]+$)|^\\.$', new$Word), select = c('Word', 'Lemma', 'Tag'))
   # Edit all words to lower case.
   new <- data.frame(lapply(new, tolower), stringsAsFactors = FALSE)
   new <- data.table(new)
-  new$nextTag <- c(new$Tag[2:(length(new$Tag))], NA)
-  new$nextWord <- c(new$Word[2:(length(new$Word))], NA)
-  newTag <- data.table(count(new, c('Tag', 'nextTag')))
-  newWord <- data.table(count(new, c('Word', 'nextWord')))
-  newLink <- data.table(new$Word, new$Tag)
-  colnames(newLink) <- c('Word', 'Tag')
-  setkey(newLink)
-  newLink <- unique(newLink)
-  if(exists('dictionaryWord')) {
-    dictionaryTag <- merge(dictionaryTag, newTag, by=c('Tag', 'nextTag'), all=TRUE)
-    dictionaryWord <- merge(dictionaryWord, newWord, by=c('Word', 'nextWord'), all=TRUE)
-    dictionaryLink <- merge(dictionaryLink, newLink, by=c('Word', 'Tag'), all=TRUE)
-    dictionaryTag <- data.table(dictionaryTag$Tag, dictionaryTag$nextTag, rowSums(dictionaryTag[, c(3,4), with=FALSE], na.rm = TRUE))
-    dictionaryWord <- data.table(dictionaryWord$Word, dictionaryWord$nextWord, rowSums(dictionaryWord[, c(3,4), with=FALSE], na.rm = TRUE))
-    colnames(dictionaryTag) <- c('Tag', 'nextTag', 'Count')
-    colnames(dictionaryWord) <- c('Word', 'nextWord', 'Count')
+  #new$nextTag <- c(new$Tag[2:(length(new$Tag))], NA)
+  new$nextLemma <- c(new$Lemma[2:(length(new$Lemma))], NA)
+  #newTag <- data.table(count(new, c('Tag', 'nextTag')))
+  newLemma <- data.table(count(new, c('Lemma', 'nextLemma')))
+  #new$nextTag <- NULL
+  new$nextLemma <- NULL
+  newLink <- unique(new)
+  if(exists('dictionaryLemma')) {
+    #dictionaryTag <- merge(dictionaryTag, newTag, by=c('Tag', 'nextTag'), all=TRUE)
+    dictionaryLemma <- merge(dictionaryLemma, newLemma, by=c('Lemma', 'nextLemma'), all=TRUE)
+    dictionaryLink <- merge(dictionaryLink, newLink, by=c('Word', 'Lemma', 'Tag'), all=TRUE)
+    #dictionaryTag <- data.table(dictionaryTag$Tag, dictionaryTag$nextTag, rowSums(dictionaryTag[, c(3,4), with=FALSE], na.rm = TRUE))
+    dictionaryLemma <- data.table(dictionaryLemma$Lemma, dictionaryLemma$nextLemma, rowSums(dictionaryLemma[, c(3,4), with=FALSE], na.rm = TRUE))
+    #colnames(dictionaryTag) <- c('Tag', 'nextTag', 'Count')
+    colnames(dictionaryLemma) <- c('Lemma', 'nextLemma', 'Count')
   } else {
-    dictionaryTag <- newTag
-    dictionaryWord <- newWord
+    #dictionaryTag <- newTag
+    dictionaryLemma <- newLemma
     dictionaryLink <- newLink
   }
 }
 
-write.table(dictionaryLink, file='dictionarylink.csv', sep=',', fileEncoding = 'UTF-8')
-write.table(dictionaryTag, file='dictionarytag.csv', sep=',', fileEncoding = 'UTF-8')
-write.table(dictionaryWord, file='dictionaryword.csv', sep=',', fileEncoding = 'UTF-8')
+write.table(dictionaryLink, file='dictionarylink1.csv', sep=',', fileEncoding = 'UTF-8')
+#write.table(dictionaryTag, file='dictionarytag1.csv', sep=',', fileEncoding = 'UTF-8')
+write.table(dictionaryLemma, file='dictionarylemma1.csv', sep=',', fileEncoding = 'UTF-8')
--- RealWordErrors.R
+++ RealWordErrors.R
@@ -4,6 +4,7 @@
 # Authors: Kjartan Marteinsson, Snorri Ágúst Snorrason, Tumi Snær Gíslason.
 ############################################################################
 library(plyr)
+library(data.table)
 
 findRealWordErrors <- function(data, csv=FALSE) {
   
@@ -18,66 +19,47 @@ findRealWordErrors <- function(data, csv=FALSE) {
   data <- subset(data, grepl('(^[[:alpha:]]+$)|^\\.$', data$Word), select = colnames(data))
   sum <- 0
   
+  dictionaryTag <- data.table(dictionaryTag)
+  dictionaryWord <- data.table(dictionaryWord)
+  
   for(i in 2:50) {
     print(data$Word[i-1])
     print(data$Word[i])
     print(data$Word[i+1])
     
-    possibleTags <- findCandidateTags(data$Tags[i - 1], data$Tags[i + 1])
-    possibleWords <- findCandidateWords(data$Word[i - 1], data$Words[i + 1])
-      
-    linkedDict <- dictionaryLink[which(dictionaryLink$Tag %in% possibleTags$Tag & dictionaryLink$Word %in% possibleWords$Word),]
+    possibleTags <- findCandidates(dictionaryTag, data$Tag[i - 1], data$Tag[i + 1])
+    possibleWords <- findCandidates(dictionaryWord, data$Word[i - 1], data$Word[i + 1])
+
+    linkedDict <- dictionaryLink[which(dictionaryLink$Tag %in% possibleTags$Candidates & dictionaryLink$Word %in% possibleWords$Candidates),]
     linkedDict['Prob'] <- ''
     linkedDictLength <- nrow(linkedDict)
+
     for (i in 1:linkedDictLength) {
-      tagProb <- possibleTags$Prob[possibleTags$Tag == linkedDict$Tag[i]]
-      wordProb <- possibleWords$Prob[possibleWords$Word == linkedDict$Word[i]]
+      tagProb <- possibleTags$Prob[possibleTags$Candidates == linkedDict$Tag[i]]
+      wordProb <- possibleWords$Prob[possibleWords$Candidates == linkedDict$Word[i]]
       linkedDict$Prob[i] <- tagProb + wordProb
     }
     print(linkedDict[which(linkedDict$Prob == max(linkedDict$Prob)),])
   }
 }
 
-findCandidateWords <- function(previousWord, nextWord) {
-  
-  countForward <- sum(dictionaryWord$Count[dictionaryWord$Word == previousWord], na.rm = TRUE)
-  countBackward <- sum(dictionaryWord$Count[dictionaryWord$Word == nextWord], na.rm = TRUE)
-  wordForward <- dictionaryWord[which(dictionaryWord$Word == previousWord),]
-  wordForward$Count <- log10(wordForward$Count/countForward)
-  wordBackward <- dictionaryWord[which(dictionaryWord$nextWord == nextWord),]
-  wordBackward$Count <- log10(wordBackward$Count/countBackward)
-  
-  colnames(wordForward)[2] <- 'WordJoin'   # nextWord
-  colnames(wordBackward)[1] <- 'WordJoin'  # Word
-  possibleWords <- data.frame(merge(wordForward, wordBackward, by='WordJoin'), stringsAsFactors = FALSE)
-  possibleWords$Count.x <- possibleWords$Count.x + possibleWords$Count.y
-  possibleWords$Count.y <- NULL
-  colnames(possibleWords) <- c('Word', 'Prob')
-  possibleWords <- possibleWords[order(possibleWords$Prob, decreasing = TRUE),]
-  possibleWords <- head(possibleWords, 100)
-  
-  return(possibleWords)
-}
+findCandidates <- function(dictionary, preciding, following) {
 
-findCandidateTags <- function(previousTag, nextTag) {
-  
-  countForward <- sum(dictionaryTag$Count[dictionaryTag$Tag == previousTag], na.rm = TRUE)
-  countBackward <- sum(dictionaryTag$Count[dictionaryTag$nextTag == nextTag], na.rm = TRUE)
-  tagsForward <- dictionaryTag[which(dictionaryTag$Tag == previousTag),]
-  tagsForward$Count <- log10(tagsForward$Count/countForward)
-  tagsBackward <- dictionaryTag[which(dictionaryTag$nextTag == nextTag),]
-  tagsBackward$Count <- log10(tagsBackward$Count/countBackward)
-  
-  colnames(tagsForward)[2] <- 'TagJoin'   # nextTag
-  colnames(tagsBackward)[1] <- 'TagJoin'  # Tag
-  possibleTags <- data.frame(merge(tagsForward, tagsBackward, by='TagJoin'), stringsAsFactors = FALSE)
-  possibleTags$Count.x <- possibleTags$Count.x + possibleTags$Count.y
-  possibleTags$Count.y <- NULL
-  colnames(possibleTags) <- c('Tag', 'Prob')
-  possibleTags <- possibleTags[order(possibleTags$Prob, decreasing = TRUE),]
-  possibleTags <- head(possibleTags, 100)
-  
-  return(possibleTags)
+  forward <- dictionary[dictionary[[1]] == preciding,]
+  forward[[3]] <- log10(forward[[3]]/sum(forward$Count, na.rm = TRUE))
+  colnames(forward)[2] <- 'join'
+
+  backward <- dictionary[dictionary[[2]] == following,]
+  backward[[3]] <- log10(backward[[3]]/sum(backward$Count, na.rm = TRUE))
+  colnames(backward)[1] <- 'join'
+
+  candidates <- data.table(merge(forward, backward, by='join'))
+  str(candidates)
+  candidates <- data.table(candidates$join, candidates$Count.x + candidates$Count.y)
+  colnames(candidates) <- c('Candidates', 'Prob')
+  candidates <- candidates[order(candidates$Prob, decreasing=TRUE),]
+  candidates <- head(candidates, 100)
+  return(candidates)
 }
 
 words <- findRealWordErrors('althingi_errors/079.csv', TRUE)
