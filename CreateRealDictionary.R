cfc97c2a9d7e90903dd33b0383fa247ec00a9c71
KjartanMa <kjartanm2@gmail.com>
2015-11-22 18:48:32 +0000

    More comments

--- CreateRealDictionary.R
+++ CreateRealDictionary.R
@@ -1,7 +1,7 @@
 ############################################################################
 # Script that reads through althingi speeches training data and
-# creates a dictionary of the words used. The dictionary contains the
-# words used and the frequency of usage for each.
+# creates a context sensative dictionaries. Creates three dictionaries
+# for lemma, tag and links.
 #
 # Authors: Kjartan Marteinsson, Snorri Ágúst Snorrason, Tumi Snær Gíslason.
 ############################################################################
@@ -11,7 +11,7 @@ library(data.table)
 # List the training files.
 files <- list.files(path = 'althingi_tagged')
 
-# Make sure the dictionary object is empty before populating it.
+# Make sure the dictionary objects is empty before populating it.
 if(exists('dictionaryTag')){
   rm(dictionaryTag)
 }
@@ -23,28 +23,34 @@ if(exists('dictionaryLemma')){
 if(exists('dictionaryLink')){
   rm(dictionaryLink)
 }
-# Read all the data files and create a dictionary out of the words
-# contained in them.
+# Read all the data files and create a dictionaries.
 for (name in files) {
-  print(name)
+  # Read in the current file.
   new <- read.csv(paste0('althingi_tagged/', name), encoding = 'UTF-8', stringsAsFactors = FALSE)
   # Remove all non letter words.
   new <- subset(new, grepl('(^[[:alpha:]]+$)|^\\.$', new$Word), select = c('Word', 'Lemma', 'Tag'))
   # Edit all words to lower case.
   new <- data.frame(lapply(new, tolower), stringsAsFactors = FALSE)
   new <- data.table(new)
+  # Make new columns with the next tags and lemmas that appear.
   new$nextTag <- c(new$Tag[2:(length(new$Tag))], NA)
   new$nextLemma <- c(new$Lemma[2:(length(new$Lemma))], NA)
+  # Count the apperance of each tag nextTag pair in the file.
   newTag <- data.table(count(new, c('Tag', 'nextTag')))
+  # Count the apperance of each lemma nextLemma pair in the file.
   newLemma <- data.table(count(new, c('Lemma', 'nextLemma')))
   new$nextTag <- NULL
   new$nextLemma <- NULL
+  # Select all unique word, lemma, tag pairs in the file.
   newLink <- unique(new)
+  # Merge values for the current file with the global dictionaries.
   if(exists('dictionaryLemma')) {
     dictionaryTag <- merge(dictionaryTag, newTag, by=c('Tag', 'nextTag'), all=TRUE)
     dictionaryLemma <- merge(dictionaryLemma, newLemma, by=c('Lemma', 'nextLemma'), all=TRUE)
     dictionaryLink <- merge(dictionaryLink, newLink, by=c('Word', 'Lemma', 'Tag'), all=TRUE)
+    # Count togather unique tag nextTag values.
     dictionaryTag <- data.table(dictionaryTag$Tag, dictionaryTag$nextTag, rowSums(dictionaryTag[, c(3,4), with=FALSE], na.rm = TRUE))
+    # Count together unique lemma nextLemma values.
     dictionaryLemma <- data.table(dictionaryLemma$Lemma, dictionaryLemma$nextLemma, rowSums(dictionaryLemma[, c(3,4), with=FALSE], na.rm = TRUE))
     colnames(dictionaryTag) <- c('Tag', 'nextTag', 'Count')
     colnames(dictionaryLemma) <- c('Lemma', 'nextLemma', 'Count')
@@ -54,7 +60,7 @@ for (name in files) {
     dictionaryLink <- newLink
   }
 }
-
-write.table(dictionaryLink, file='dictionarylink1.csv', sep=',', fileEncoding = 'UTF-8')
-write.table(dictionaryTag, file='dictionarytag1.csv', sep=',', fileEncoding = 'UTF-8')
-write.table(dictionaryLemma, file='dictionarylemma1.csv', sep=',', fileEncoding = 'UTF-8')
+# Save the dictionaries in files
+write.table(dictionaryLink, file='dictionarylink.csv', sep=',', fileEncoding = 'UTF-8')
+write.table(dictionaryTag, file='dictionarytag.csv', sep=',', fileEncoding = 'UTF-8')
+write.table(dictionaryLemma, file='dictionarylemma.csv', sep=',', fileEncoding = 'UTF-8')
